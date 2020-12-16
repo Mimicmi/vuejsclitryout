be8e096c54e2f7d796a9d16f9a1a38a54c338543
Simon Frezard <simon.frezard@gmail.com>
2020-12-16 11:54:32 +0100

    first  component

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <!--zone html-->
+    <h1>Hello VUE 3</h1>
+    <h2>{{ title }}</h2>
+    <img src="../assets/logo.png" alt="">
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                title: "Test titre OMEGALUL",
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        }
+    }
+</script>
+
+<style scoped>
+    /*zone css, le scoped mets le css QUE pour ce composant*/
+    h1 {
+        color: red;
+    }
+</style>
